435d9e1b089198c393405434fc58ffada0e905d0
赵龙生 <zoeyzhao19@gmail.com>
2023-08-09 12:06:15 +0800

    chore: keep cache always valid

--- src/cache.ts
+++ src/cache.ts
@@ -1,25 +1,16 @@
 // https://github.com/antfu/taze/src/io/resolves.ts
 import { resolve } from 'node:path'
 import os from 'node:os'
-import { existsSync, promises as fs, lstatSync } from 'node:fs'
+import { existsSync, promises as fs } from 'node:fs'
 import { consola } from 'consola'
 
 let cache: Record<string, Record<string, any>> = {}
 
 const cacheDir = resolve(os.tmpdir(), 'wonder-dep')
 const cachePath = resolve(cacheDir, 'cache.json')
-const cacheTTL = 30 * 60_000 // 30min
-
-function now() {
-  return +new Date()
-}
-
-function ttl(n: number) {
-  return now() - n
-}
 
 export async function loadCache() {
-  if (existsSync(cachePath) && ttl(lstatSync(cachePath).mtimeMs) < cacheTTL) {
+  if (existsSync(cachePath)) {
     consola.info(`cache loaded from ${cachePath}`)
     cache = JSON.parse(await fs.readFile(cachePath, 'utf-8'))
   }
