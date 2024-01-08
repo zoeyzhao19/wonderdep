d3086ba9c4d44fe3820f116354e444e164fc5b72
赵龙生 <zoeyzhao19@gmail.com>
2024-01-08 15:13:05 +0800

    chore: update

--- test/resolve.test.ts
+++ test/resolve.test.ts
@@ -2,17 +2,6 @@ import { describe, expect, test } from 'vitest'
 import { resolveHostVersion } from '../src'
 
 describe('resolve', async () => {
-  // test('resolve a specify dep version from a specify host package ', async () => {
-  //   const data: [string, string[]] = ['vite-plugin-inspect@0.7.35', ['vite']]
-
-  //   const result = await resolveDepsVersion(data[0], data[1])
-  //   expect(result).toMatchInlineSnapshot(`
-  //     [
-  //       "vite-plugin-inspect@0.7.35 => vite^4.4.8",
-  //     ]
-  //   `)
-  // })
-
   test('resolve specify host package version from a specify dep', async () => {
     const data: [string, string[]] = ['vite-plugin-inspect', ['vite@4.4.8']]
 
